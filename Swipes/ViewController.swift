e3d0c37ec9b0a0a81416c575d0dbb5fcbcb8dc3f
danielCatlett <daniel.catlett42@gmail.com>
2017-05-10 15:49:57 -0500

    Swipes first round

--- Swipes/ViewController.swift
+++ Swipes/ViewController.swift
@@ -1,25 +1,61 @@
-//
 //  ViewController.swift
 //  Swipes
 //
 //  Created by Daniel Catlett on 5/10/17.
 //  Copyright © 2017 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet var label: UILabel!
+    private var gestureStartPoint:CGPoint!
+    private static let minimumGestureLength = Float(25.0)
+    private static let maximumVariance = Float(5)
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if let touch = touches.first
+        {
+            gestureStartPoint = touch.location(in: self.view)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if let touch = touches.first, let gestureStartPoint = self.gestureStartPoint
+        {
+            let currentPosition = touch.location(in: self.view)
+            
+            let deltaX = fabsf(Float(gestureStartPoint.x - currentPosition.x))
+            let deltaY = fabsf(Float(gestureStartPoint.y - currentPosition.y))
+            
+            if(deltaX >= ViewController.minimumGestureLength && deltaY <= ViewController.maximumVariance)
+            {
+                label.text = "Horizontal swipe detected"
+                DispatchQueue.main.after(when: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC))
+                {
+                    self.label.text = ""
+                }
+            }
+            else if(deltaY >= ViewController.minimumGestureLength && deltaX <= ViewController.maximumVariance)
+            {
+                label.text = "Vertical swipe detected"
+                DispatchQueue.main.after(when: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC))
+                {
+                    self.label.text = ""
+                }
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
